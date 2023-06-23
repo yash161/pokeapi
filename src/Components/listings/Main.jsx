67d0815620dd447a9e08ff3a228ff2cb1683afcd
yash161 <yashshah19@gnu.ac.in>
2023-06-23 14:26:21 +0530

    adding scroll

--- src/Components/listings/Main.jsx
+++ src/Components/listings/Main.jsx
@@ -1,44 +1,68 @@
-import React from 'react'
-import Card from './Card'
-import Pokeinfo from './Pokeinfo'
-import axios from 'axios'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import Card from './Card';
+import Pokeinfo from './Pokeinfo';
+import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 const Main = () => {
-  const [pokeData, setPokeData] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/')
-  const [nextUrl, setNextUrl] = useState()
-  const [prevUrl, setPrevUrl] = useState()
-  const [pokeDex, setPokeDex] = useState()
+  const [pokeData, setPokeData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/');
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [pokeDex, setPokeDex] = useState('');
+  const [allPokeData, setAllPokeData] = useState([]);
+  const [scrollPosition, setScrollPosition] = useState(0); // Track scroll position
 
   const pokeFun = async () => {
-    setLoading(true)
-    const res = await axios.get(url)
-    setNextUrl(res.data.next)
-    setPrevUrl(res.data.previous)
-    getPokemon(res.data.results)
-    setLoading(false)
-  }
+    setLoading(true);
+    const res = await axios.get(url);
+    setNextUrl(res.data.next);
+    setPrevUrl(res.data.previous);
+    getPokemon(res.data.results);
+    setLoading(false);
+  };
+
   const getPokemon = async (res) => {
-    res.map(async (item) => {
-      const result = await axios.get(item.url)
-      setPokeData((state) => {
-        state = [...state, result.data]
-        state.sort((a, b) => (a.id > b.id ? 1 : -1))
-        return state
-      })
-    })
-  }
+    for (let i = 0; i < res.length; i++) {
+      const result = await axios.get(res[i].url);
+      setAllPokeData((state) => {
+        state = [...state, result.data];
+        state.sort((a, b) => (a.id > b.id ? 1 : -1));
+        return state;
+      });
+    }
+  };
+
   useEffect(() => {
-    pokeFun()
-  }, [url])
+    pokeFun();
+    const storedPosition = localStorage.getItem('scrollPosition');
+    if (storedPosition) {
+      window.scrollTo(0, parseInt(storedPosition));
+    }
+    return () => {
+      localStorage.setItem('scrollPosition', window.pageYOffset.toString());
+    };
+  }, [url]);
+
+  const loadMoreData = () => {
+    if (nextUrl) {
+      const currentPosition = window.pageYOffset; // Get current scroll position
+      setUrl(nextUrl);
+      setScrollPosition(currentPosition); // Store the current scroll position
+    }
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, scrollPosition); // Restore scroll position after rendering new content
+  }, [allPokeData]);
+
   return (
     <>
       <div className="container">
         <div className="left-content">
           <Card
-            pokemon={pokeData}
+            pokemon={allPokeData}
             loading={loading}
             infoPokemon={(poke) => setPokeDex(poke)}
           />
@@ -47,8 +71,7 @@ const Main = () => {
             {prevUrl && (
               <button
                 onClick={() => {
-                  setPokeData([])
-                  setUrl(prevUrl)
+                  setUrl(prevUrl);
                 }}
               >
                 Previous
@@ -56,14 +79,12 @@ const Main = () => {
             )}
 
             {nextUrl && (
-              <button
-                onClick={() => {
-                  setPokeData([])
-                  setUrl(nextUrl)
-                }}
-              >
-                Next
-              </button>
+              <InfiniteScroll
+                dataLength={allPokeData.length}
+                next={loadMoreData}
+                hasMore={true}
+                loader={<div className="loading-spinner"></div>}
+              />
             )}
           </div>
         </div>
@@ -72,6 +93,7 @@ const Main = () => {
         </div>
       </div>
     </>
-  )
-}
-export default Main
+  );
+};
+
+export default Main;
